f8e57fd22885cbde1ec9d1382df57e4f0edf9ba7
Abdillahi-A <39855630+Abdillahi-A@users.noreply.github.com>
2019-05-23 23:32:54 +0100

    Update atm.py

--- atm.py
+++ atm.py
@@ -13,11 +13,17 @@ def options():
 
 def deposit():
     global account_funds, statement
-    deposit_amount = float(input("How much would you like to deposit?\n"))
-    account_funds += deposit_amount
-    print(f"Success: you have deposited £{deposit_amount:.2f}. Your new balance is £{account_funds:.2f}.")
-    t = time.time()
-    log_statement(t, deposit_amount)
+    try:
+        deposit_amount = float(input("How much would you like to deposit?\n"))
+        if deposit_amount > 0:
+            account_funds += deposit_amount
+            print(f"Success: you have deposited £{deposit_amount:.2f}. Your new balance is £{account_funds:.2f}.")
+            t = time.time()
+            log_statement(t, deposit_amount)
+        else:
+            print("Please make sure you deposit an amount greater than 0.")
+    except ValueError:
+        print("Oops that didn't work. Please make sure you enter a number.")
     
 def log_statement(t, deposit_amount):
     global account_funds, statement
@@ -25,9 +31,12 @@ def log_statement(t, deposit_amount):
 
 def display_statement():
     global statement
-    for item in statement:
-        t,deposit_amount, account_funds = item
-        print(f"Date:{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(t))}, Transaction:{deposit_amount:.2f}, Balance:{account_funds:.2f}")
+    if len(statement) == 0:
+        print("There has been no activity on this account as of yet.")
+    else:
+        for item in statement:
+            t,deposit_amount, account_funds = item
+            print(f"Date:{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(t))}, Transaction:{deposit_amount:.2f}, Balance:{account_funds:.2f}")
 
 def show_balance():
     global account_funds
@@ -35,21 +44,27 @@ def show_balance():
 
 def withdraw():
     global account_funds, statement
-    withdraw_amount = float(input("How much would you like to withdraw?\n"))
-    if withdraw_amount > account_funds:
-        print(f"Sorry this transaction could not be completed as you only have £{account_funds:.2f} available to withdraw. Please try again.")
-    else:
-        account_funds -= withdraw_amount
-        print(f"Success: you have withdrawn £{withdraw_amount:.2f}. Your new balance is £{account_funds:.2f}.")
-        t = time.time()
-        log_statement(t, -withdraw_amount)
+    try:
+        withdraw_amount = float(input("How much would you like to withdraw?\n"))
+        if withdraw_amount > account_funds:
+            print(f"Sorry this transaction could not be completed as you only have £{account_funds:.2f} available to withdraw.")
+        elif withdraw_amount<1:
+            print("Sorry the minimum amount you can withhdraw is £1.")
+        else:
+            account_funds -= withdraw_amount
+            print(f"Success: you have withdrawn £{withdraw_amount:.2f}. Your new balance is £{account_funds:.2f}.")
+            t = time.time()
+            log_statement(t, -withdraw_amount)
+    except ValueError:
+        print("Oops that didn't work. Please make sure you enter a number.")
+
     
 def return_to_options():
     go_again = input("Enter 'Y' if you'd like to return to the main menu (OR any other key to exit)\n")
     if len(go_again)>=1 and go_again[0].lower() == 'y':
         return True
-    else:
-        False
+    
+    return False
 
 def main():
     global account_funds, statement
@@ -59,24 +74,21 @@ def main():
         if option == 1:
             show_balance()
             if not return_to_options():
-                print("Thanks for using our ATM. Bye!")
                 break            
         elif option == 2:
             deposit()
             if not return_to_options():
-                print("Thanks for using our ATM. Bye!")
                 break    
         elif option == 3:
             withdraw()
             if not return_to_options():
-                print("Thanks for using our ATM. Bye!")
                 break    
         elif option == 4:
             display_statement()
             if not return_to_options():
-                print("Thanks for using our ATM. Bye!")
                 break    
         else:
             print("Sorry, that is not a valid option. Please try again.")
+    print("Thanks for using our ATM. Bye!")
 
 main()
